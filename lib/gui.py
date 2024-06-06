20853ed66290e9fa7ef48655eb549e82c9a10fe6
Kostevich Arseniy <kostevich_a@bk.ru>
2024-06-06 21:56:13 +0300

    Added combobox to change criterium

--- lib/gui.py
+++ lib/gui.py
@@ -15,18 +15,18 @@ class PlotData(FigureCanvasQTAgg):
     '''
     Виджет для отрисовки графиков, использующий matplotlib
     '''
-    def __init__(self, parent=None, df=None, axe_x="Id1", axe_y="I-131", type="barplot", title=None):
+    def __init__(self, parent=None):
         sns.set(style="whitegrid", context="paper")
         self.fig = plt.figure(figsize=(15, 10))
         self.axes = self.fig.add_subplot(111)
-        #self.draw_plot(df, axe_x, axe_y, type)
+        #self.draw_plot(df, axe_x, axe_y, type, title)
 
         super(PlotData, self).__init__(self.fig)
     
     def draw_plot(self, df, axe_x="Id1", axe_y="I-131", type = "barplot", title=None):
         self.axes.cla()
         getattr(sns,type)(data=df, x=axe_x, y=axe_y, ax=self.axes)
-        plt.title(label=title, fontsize=18)
+        plt.title(label=title, fontsize=16)
 
 class PenalWidget(QtWidgets.QWidget):
     def __init__():
@@ -38,7 +38,11 @@ class MainWindow(QtWidgets.QMainWindow):
         #self.resize(1200,900)
         self.model=model
 
-        self.sc = PlotData(self, df = self.model.penals[0].data)
+        self.id = 0
+        self.title = ""
+        self.axe_y = "I-131"
+
+        self.sc = PlotData(self)
 
         layout = QtWidgets.QHBoxLayout()
         self.penal_menu = QtWidgets.QWidget()
@@ -46,14 +50,20 @@ class MainWindow(QtWidgets.QMainWindow):
         for i in range(0,len(self.model.data.penals_id)):
             btn_name = "Пенал %s" % self.model.data.penals_id[i]
             penal_btn = QtWidgets.QPushButton(btn_name)
-            penal_btn.released.connect(lambda id=i, name=btn_name: self.update_plot(df = self.model.penals[id].data, title = name))
+            penal_btn.released.connect(lambda id=i, name=btn_name: self.change_penal(title = name, id = id))
             layout.addWidget(penal_btn)
 
+        self.criterium = QtWidgets.QComboBox()
+        self.criterium.addItems(["I-131", "Cs-134", "Cs-137", "Cs-136", "Xe-133","Mn-54"])
+        self.criterium.activated[str].connect(lambda axe_y=str: self.update_plot(axe_y=axe_y))
+
+        layout.addWidget(self.criterium)
         self.penal_menu.setLayout(layout)
 
         toolbar = NavigationToolbar(self.sc, self)
 
         self.layout = QtWidgets.QVBoxLayout()
+        
         self.layout.addWidget(toolbar)
         self.layout.addWidget(self.sc)
         self.layout.addWidget(self.penal_menu)
@@ -63,6 +73,13 @@ class MainWindow(QtWidgets.QMainWindow):
 
         self.setCentralWidget(self.widget)
 
-    def update_plot(self, df, axe_x="Id1", axe_y="I-131", type = "barplot", title=None):
-        self.sc.draw_plot(df=df, axe_x=axe_x, axe_y=axe_y, type=type, title=title)
-        self.sc.draw()
+    def change_penal(self, title, id):
+        self.title = title
+        self.id = id
+        self.update_plot()
+
+    def update_plot(self, axe_x="Id1", axe_y=None, type = "barplot"):
+        if axe_y: self.axe_y = axe_y
+        if self.title != "":
+            self.sc.draw_plot(self.model.penals[self.id].data, axe_x=axe_x, axe_y=self.axe_y, type=type, title=self.title)
+            self.sc.draw()
--- lib/main.py
+++ lib/main.py
@@ -1,16 +1,21 @@
 from PyQt5 import QtWidgets
 from model import Model
 from gui import MainWindow, open_file
+import pandas as pd
+import numpy as np
 import sys
 
 if __name__ == "__main__":
     app = QtWidgets.QApplication(sys.argv)
     
-    #filename = open_file()
+    filename = open_file()
 
     model = Model()
-    
+    # model.control(0)
+    # model.control(1)
+    #model.control(0)
     main_window = MainWindow(model)
     main_window.show()
 
     app.exec_()
+
